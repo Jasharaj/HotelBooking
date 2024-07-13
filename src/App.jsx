9fddd273dad7168ab7a016dee2435468d011984e
Jasharaj <jasharaj38624@outlook.com>
2024-07-13 17:03:09 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Home from '../Pages/Home'
+import './App.css'
+import Nav from './Components/Nav'
+
+function App() {
+  return (
+    <div className='relative'>
+      <Nav />
+      <Home />
+    </div>
+  )
+}
+
+export default App
