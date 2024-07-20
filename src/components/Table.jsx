cce6cc40bb56cbfbf454057c6f6a3fa75268304d
Jasharaj <jasharaj38624@outlook.com>
2024-07-20 20:37:25 +0530

    Solve cancel issue

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,73 +5,16 @@ import appwriteService1 from "../appwrite/config.js";
 import appwriteService2 from "../appwrite/count.js";
 
 
-const cancelBooking = async (docId, name, guests, startdate, enddate) => {
-  await appwriteService1.deleteBooking(docId);
-  const s = startdate.slice(0, 2);
-  let e = enddate.slice(0, 2);
-  const m = startdate.slice(2);
-  const m2 = enddate.slice(2);
-
-  if (m2 > m) {
-    //For two month query
-    for (let i = s; i <= "31"; i++) {
-      const slot = await appwriteService2.getSlots(name, i + m);
-      const userId = slot.documents[0].$id;
-      await appwriteService2.updateSlots(
-        userId,
-        slot.documents[0].slots + guests
-      );
-    }
-
-    for (let i = "01"; i <= e; i++) {
-      let i2 = "0" + i;
-      let i3 = i2.slice(-2);
-      const slot = await appwriteService2.getSlots(name, i3 + m2);
-      console.log(i3 + m2);
-      const userId = slot.documents[0].$id;
-      await appwriteService2.updateSlots(
-        userId,
-        slot.documents[0].slots + guests
-      );
-    }
-  } else {
-    for (let i = s; i <= e; i++) {
-      const slot = await appwriteService2.getSlots(name, i + m);
-      const userId = slot.documents[0].$id;
-      await appwriteService2.updateSlots(
-        userId,
-        slot.documents[0].slots + guests
-      );
-    }
-  }
-};
-
 function Table({ docId, num, name, guests, startdate, enddate }) {
-  const [msg, setMsg] = useState("❌Cancel ");
-  const [runOnce, setrunOnce] = useState(true);
+
   return (
-    <tr className="hover">
+    <>
       <th>{num}</th>
       <td>{name}</td>
       <td>{guests}</td>
       <td>{startdate}</td>
-      <td>{enddate}</td>
-      <td
-        onClick={() => {
-          if (runOnce) {
-            cancelBooking(docId, name, guests, startdate, enddate);
-            setMsg("Cancelled");
-            setrunOnce(false);
-          } else {
-            {
-              console.log("Kitni bar cancel karoge");
-            }
-          }
-        }}
-      >
-        <Button >{msg}</Button>
-      </td>
-    </tr>
+      <td>{enddate}</td> 
+    </>
   );
 }
 
--- src/pages/YourBookings.jsx
+++ src/pages/YourBookings.jsx
@@ -7,7 +7,8 @@ import appwriteService2 from "../appwrite/count.js";
 
 function YourBookings() {
   const [bookings, setBookings] = useState([]);
-  // const [num, setNum] = useState(1);
+  const [msg, setMsg] = useState("❌Cancel ");
+  const [loading,setLoading] = useState(false)
 
   const id = useId();
   let num = 0;
@@ -16,18 +17,73 @@ function YourBookings() {
     const user = await authService.getCurrentUser();
     const userId = user.$id;
     appwriteService1.getBookings(userId).then((t) => {
-      if (t) {
+      if (t.documents.length == 0) {
+        setBookings([
+          {
+            $id: "NO BOOKINGS",
+            name: "NO BOOKINGS",
+            guests: "NO BOOKINGS",
+            startdate: "NO BOOKINGS",
+            enddate: "NO BOOKINGS",
+          },
+        ]);
+      } else {
         setBookings(t.documents);
       }
     });
-  }
+    setLoading(false)
+  };
+
+  const cancelBooking = async (docId, name, guests, startdate, enddate) => {
+    await appwriteService1.deleteBooking(docId);
+    const s = startdate.slice(0, 2);
+    let e = enddate.slice(0, 2);
+    const m = startdate.slice(2);
+    const m2 = enddate.slice(2);
+
+    if (m2 > m) {
+      //For two month query
+      for (let i = s; i <= "31"; i++) {
+        const slot = await appwriteService2.getSlots(name, i + m);
+        const userId = slot.documents[0].$id;
+        await appwriteService2.updateSlots(
+          userId,
+          slot.documents[0].slots + guests
+        );
+      }
+
+      for (let i = "01"; i <= e; i++) {
+        let i2 = "0" + i;
+        let i3 = i2.slice(-2);
+        const slot = await appwriteService2.getSlots(name, i3 + m2);
+        console.log(i3 + m2);
+        const userId = slot.documents[0].$id;
+        await appwriteService2.updateSlots(
+          userId,
+          slot.documents[0].slots + guests
+        );
+      }
+    } else {
+      for (let i = s; i <= e; i++) {
+        const slot = await appwriteService2.getSlots(name, i + m);
+        const userId = slot.documents[0].$id;
+        await appwriteService2.updateSlots(
+          userId,
+          slot.documents[0].slots + guests
+        );
+      }
+    }
+    
+    getUser()
+
+  };
+
 
   useEffect(() => {
-    getUser()
-  }, [])
-  
+    getUser();
+  }, []);
 
-  if (bookings.length == 0) {
+  if (bookings.length == 0 || loading==true) {
     return (
       <div className="w-full min-h-[80vh] py-8">
         <Loadinglg />
@@ -60,15 +116,31 @@ function YourBookings() {
                 num = num + 1;
               }
               return (
-                <Table
-                  key={id}
-                  docId={t.$id}
-                  num={num}
-                  name={t.name}
-                  guests={t.guests}
-                  startdate={t.startdate}
-                  enddate={t.enddate}
-                />
+                <tr className="hover">
+                  <Table
+                    key={id}
+                    docId={t.$id}
+                    num={num}
+                    name={t.name}
+                    guests={t.guests}
+                    startdate={t.startdate}
+                    enddate={t.enddate}
+                  />
+                  <td
+                    onClick={() => {
+                      if (t.$id != "NO BOOKINGS") {
+                        cancelBooking(t.$id,t.name,t.guests, t.startdate, t.enddate);
+                        setLoading(true)
+                      } else {
+                        {
+                          setMsg("NO BOOKINGS");
+                        }
+                      }
+                    }}
+                  >
+                    <Button>{msg}</Button>
+                  </td>
+                </tr>
               );
             })}
           </tbody>
